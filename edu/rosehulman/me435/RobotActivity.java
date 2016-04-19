e55c427ca5e67955105c7c316672d327d06f953b
David Fisher <fisherds@rose-hulman.edu>
2016-04-19 00:38:40 -0400

    Handling new FieldGps permission system in RobotActivity

--- edu/rosehulman/me435/RobotActivity.java
+++ edu/rosehulman/me435/RobotActivity.java
@@ -253,6 +253,12 @@ public class RobotActivity extends AccessoryActivity implements FieldGpsListener
   }
 
   @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    mFieldGps.requestLocationUpdates(this);
+  }
+
+  @Override
   protected void onStart() {
     super.onStart();
     mTimer = new Timer();
